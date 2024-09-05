5e9d837096ec2e1aced8d54f4b4ae353bed37dd8
gelotuason <gelotuason@gmail.com>
2024-09-06 00:05:27 +0800

    added vector and adjusted responsiveness

--- dnw_project/tailwind.config.js
+++ dnw_project/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       fontFamily: {
         bebas: ['Bebas Neue', 'sans-serif'],
         segoe: ['Segoe UI', 'sans-serif']
+      },
+      screens: {
+        'w-375': '375px',
       }
     },
   },
